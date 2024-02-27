222eea59f4a5790ae8aeb71df9788834ca40a846
tauri_s <tauristclaire@gmail.com>
2024-02-26 16:37:50 -0800

    Add html and PHP to content.php to bring in date and copy comments tag

--- content.php
+++ content.php
@@ -13,6 +13,7 @@
 <article id="post-<?php the_ID(); ?>" class="post-entry">
 	<div class="entry-wrap">
 		<header class="entry-header">
+			<p class="entry-time"><?php the_date(); ?></a></p>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<div class="entry-summary">
@@ -20,6 +21,9 @@
 		</div>
 		<footer class="entry-footer">
 			<div class="entry-meta">
+				<span class="entry-terms comments author">
+					<a href="<?php comments_link(); ?>"><?php comments_number( '<span>No Comments</span>', '1 Comment', '% comments' ); ?></a>
+				</span>
 				<span class="entry-terms comments author">
 					<a href="<?php comments_link(); ?>"><?php comments_number( '<span>No Comments</span>', '1 Comment', '% comments' ); ?></a>
 				</span>
