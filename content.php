15ef9f32403dd1de77b5d7d7a9219a1e53924852
tauri_s <tauristclaire@gmail.com>
2024-02-26 16:43:20 -0800

    Add php tags for the author in entry terms in content.php

--- content.php
+++ content.php
@@ -13,7 +13,7 @@
 <article id="post-<?php the_ID(); ?>" class="post-entry">
 	<div class="entry-wrap">
 		<header class="entry-header">
-			<p class="entry-time"><?php the_date(); ?></a></p>
+			<p class="entry-time"><?php the_date(); ?></p>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<div class="entry-summary">
@@ -22,7 +22,7 @@
 		<footer class="entry-footer">
 			<div class="entry-meta">
 				<span class="entry-terms comments author">
-					<a href="<?php comments_link(); ?>"><?php comments_number( '<span>No Comments</span>', '1 Comment', '% comments' ); ?></a>
+					<p><?php the_author(); ?></p>
 				</span>
 				<span class="entry-terms comments author">
 					<a href="<?php comments_link(); ?>"><?php comments_number( '<span>No Comments</span>', '1 Comment', '% comments' ); ?></a>
