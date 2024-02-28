ea810cf4a962d9daad20179a1b2701f853e6bedd
tauri_s <tauristclaire@gmail.com>
2024-02-27 17:45:36 -0800

    Rename fetched template part in if while loop to be content instead of content-category

--- category.php
+++ category.php
@@ -19,7 +19,7 @@ get_header(); ?>
 	<div class="main-content">
 		<?php if ( have_posts()) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content-category', get_post_format() ); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
 		<?php else: ?>
 			<article>
